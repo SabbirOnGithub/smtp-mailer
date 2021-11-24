b09bf005319bcb31eced5d7243332862b88ec53e
Sabbir <sabbir.ahmed.dev@gmail.com>
2021-11-24 11:20:08 +0600

    Mailkit modifiaction

--- MailSendSmtp/Controllers/SendMailController.cs
+++ MailSendSmtp/Controllers/SendMailController.cs
@@ -3,9 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Mail;
 using System.Threading.Tasks;
+using MailKit.Security;
 using MailSendSmtp.Models;
+using MimeKit;
+using MimeKit.Text;
+using MailKit.Net.Smtp;
 
 namespace MailSendSmtp.Controllers
 {
@@ -24,16 +27,16 @@ namespace MailSendSmtp.Controllers
             string subject = email.Subject;
             string body = email.Body;
 
-            MailMessage mm = new MailMessage();
-            mm.To.Add(new MailAddress(to));
+            var mm = new System.Net.Mail.MailMessage();
+            mm.To.Add(new System.Net.Mail.MailAddress(to));
             mm.Subject = subject;
             mm.Body = body;
-            mm.From = new MailAddress("sabbir.ahmed.dev@gmail.com");
+            mm.From = new System.Net.Mail.MailAddress("sabbir.ahmed.dev@gmail.com");
             mm.IsBodyHtml = false;
 
             try
             {
-                SmtpClient client = new SmtpClient("smpt.gmail.com")
+                var client = new System.Net.Mail.SmtpClient("smpt.gmail.com")
                 {
                     Port = 587,
                     UseDefaultCredentials = false,
@@ -41,6 +44,7 @@ namespace MailSendSmtp.Controllers
                     Credentials = new System.Net.NetworkCredential("sabbir.ahmed.dev@gmail.com", "ngbwxkwrmdhadgec")
                 };
                 client.Send(mm);
+                client.Dispose();
                 ViewBag.message = "This mail has been sent to " + email.To + " Successfully...!";
             }
             catch (Exception e)
@@ -50,6 +54,52 @@ namespace MailSendSmtp.Controllers
                 ViewBag.message = e.ToString();
             }
 
+            
+            return View();
+        }
+
+
+        public IActionResult MailKitMail()
+        {
+            var model = new MailModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult MailKitMail(MailModel mail)
+        {
+            // create email message
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(mail.From.Trim()));
+            email.To.Add(MailboxAddress.Parse(mail.To.Trim()));
+            email.Subject = mail.Subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = "<h1>"+ mail.Body +"</h1>" };
+
+            try
+            {
+                using var smtp = new SmtpClient();
+                if (mail.IsTls)
+                {
+                    smtp.Connect(mail.Host.Trim(), mail.Port, SecureSocketOptions.StartTls);
+                }
+                else
+                {
+                    smtp.Connect(mail.Host.Trim(), mail.Port, false);
+                }
+
+                smtp.Authenticate(mail.UserName.Trim(), mail.Password.Trim());
+                smtp.Send(email);
+                smtp.Disconnect(true);
+                ViewBag.message = "This mail has been sent to " + email.To + " Successfully...!";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                System.Diagnostics.Trace.WriteLine(e.ToString());
+                ViewBag.message = e.ToString();
+            }
+
+            // send email
             
             return View();
         }
---
+++ MailSendSmtp/Models/MailModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailSendSmtp.Models
+{
+    public class MailModel
+    {
+        public MailModel()
+        {
+            IsTls = true;
+            From = "sabbir.ahmed.dev@gmail.com";
+            Port = 587;
+            Host = "smtp.gmail.com";
+            UserName = "sabbir.ahmed.dev@gmail.com";
+            Password = "ngbwxkwrmdhadgec";
+        }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public bool IsTls { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
